cd86210c984b5fd3a68872989f111de438a66a0b
yungtiec <tctammychu@gmail.com>
2016-06-28 18:32:04 -0400

    npm install worked!!!

--- routes/index.js
+++ routes/index.js
@@ -8,29 +8,43 @@ var Promise = require('bluebird');
 
 router.get('/', function(req, res, next) {
 
-  var findingHotels = Hotel.findAll({
-    include: [Place]
-  });
+  // var findingHotels = Hotel.findAll({
+  //   include: [Place]
+  // });
 
-  var findingActivities = Activity.findAll({
-    include: [Place]
-  });
+  // var findingActivities = Activity.findAll({
+  //   include: [Place]
+  // });
 
-  var findingRestaurants = Restaurant.findAll({
-    include: [Place]
-  });
+  // var findingRestaurants = Restaurant.findAll({
+  //   include: [Place]
+  // });
 
-  Promise.all([
-    findingHotels,
-    findingActivities,
-    findingRestaurants
-  ])
-  .spread(function(hotels, activities, restaurants) {
-    res.render('index', {
-      hotels: hotels,
-      activities: activities,
-      restaurants: restaurants
-    });
+  // Promise.all([
+  //   findingHotels,
+  //   findingActivities,
+  //   findingRestaurants
+  // ])
+  // .spread(function(hotels, activities, restaurants) {
+  //   res.render('index', {
+  //     hotels: hotels,
+  //     activities: activities,
+  //     restaurants: restaurants
+  //   });
+  // })
+  var databaseCollection = {};
+  var placePromise = Place.findAll({});
+  var hotelPromise = Hotel.findAll({});
+  var restaurantPromise = Restaurant.findAll({});
+  var activityPromise = Activity.findAll({});
+
+  Promise.all([placePromise, hotelPromise, restaurantPromise, activityPromise])
+  .then(function(promiseArr){
+    databaseCollection['places'] = promiseArr[0];
+    databaseCollection['hotels'] = promiseArr[1];
+    databaseCollection['restaurants'] = promiseArr[2];
+    databaseCollection['activities'] = promiseArr[3];
+    res.render('index', databaseCollection);
   })
   .catch(next);
 
